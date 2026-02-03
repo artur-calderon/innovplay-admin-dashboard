e34976ed02fadd66cc56c49bf908f6abbf03ccdb
Moisés Eduardo <moises.eduardogc@gmail.com>
2026-02-03 12:09:47 -0300

    ajuste olimpiadas

--- src/pages/Olimpiadas.tsx
+++ src/pages/Olimpiadas.tsx
@@ -1161,7 +1161,7 @@ export default function Olimpiadas() {
           </CardContent>
         </Card>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
           {sortedOlimpiadas.map((olimpiada) => (
             <OlimpiadaCard
               key={olimpiada.id}
