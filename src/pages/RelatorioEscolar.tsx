f159cddea915c11963ef678b67dc8707126af4b7
Moisés Eduardo <118224290+MoisesE2@users.noreply.github.com>
2026-03-03 16:45:05 -0300

    filtros mobile

--- src/pages/RelatorioEscolar.tsx
+++ src/pages/RelatorioEscolar.tsx
@@ -2500,7 +2500,7 @@ export default function RelatorioEscolar() {
   }
 
   return (
-    <div className="container mx-auto p-6 space-y-6">
+    <div className="w-full min-w-0 space-y-6">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div>
